2b46b28e39afa6ddeeb9b20ec55f7d852b4fa1a0
Mark Hamstra <hello@markhamstra.com>
2014-03-25 16:34:14 +0100

    Minor lexicon change

--- core/components/quickstartbuttons/lexicon/en/dashboards.inc.php
+++ core/components/quickstartbuttons/lexicon/en/dashboards.inc.php
@@ -1,4 +1,4 @@
 <?php
 
 $_lang['quickstartbuttons.widget'] = "Quickstart Buttons";
-$_lang['quickstartbuttons.widget_desc'] = "The default Quickstart Buttons widget";
+$_lang['quickstartbuttons.widget_desc'] = "The Quickstart Buttons widget";
