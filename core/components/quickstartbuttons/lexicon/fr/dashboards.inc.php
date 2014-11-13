bd73231b80bccfe576d6c345af8bf56dc75bb8ea
Romain Fallet <romain.fallet@outlook.com>
2014-11-13 14:23:52 +0100

    Fr lexicon corrections

--- core/components/quickstartbuttons/lexicon/fr/dashboards.inc.php
+++ core/components/quickstartbuttons/lexicon/fr/dashboards.inc.php
@@ -1,4 +1,4 @@
 <?php
 
-$_lang['quickstartbuttons.widget'] = "Actions rapides";
+$_lang['quickstartbuttons.widget'] = "Boutons d'actions rapides";
 $_lang['quickstartbuttons.widget_desc'] = "Le widget des actions rapides";
