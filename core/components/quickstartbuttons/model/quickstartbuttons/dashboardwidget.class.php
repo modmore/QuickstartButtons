bd15ced8c2d0ba8e26b86af2799ec67cb6cb3d45
Mark Hamstra <hello@markhamstra.com>
2016-03-07 23:17:39 +0100

    Fix issue displaying the set name due to caching [#23]

--- core/components/quickstartbuttons/model/quickstartbuttons/dashboardwidget.class.php
+++ core/components/quickstartbuttons/model/quickstartbuttons/dashboardwidget.class.php
@@ -30,19 +30,88 @@ class QuickstartButtonsDashboardWidget extends modDashboardWidgetInterface {
         $cacheLifetime = 86400;
 
         // get output
+        $set = $this->getActiveSet();
         $output = $this->modx->cacheManager->get($cacheKey);
         if(empty($output)) {
-            $output = $this->getDashboardButtons();
+            $output = $this->getDashboardButtons($set);
             $this->modx->cacheManager->set($cacheKey, $output, $cacheLifetime);
         }
 
 		return $output;
 	}
 
-    public function getDashboardButtons()
+    /**
+     * @param qsbSet $set
+     * @return string
+     */
+    public function getDashboardButtons(qsbSet $set)
+    {
+        $output = '';
+
+        // collect all buttons in set
+        $c = $this->modx->newQuery('qsbButton');
+        $c->select(array('qsbButton.*', 'Icon.class AS iconcls', 'Icon.path as iconpath'));
+        $c->leftJoin('qsbIcon', 'Icon');
+        $c->where(array('active' => true));
+        $c->sortby('ranking ASC, id', 'ASC');
+
+        $buttons = $set->getMany('Button', $c);
+        if(!empty($buttons)) {
+
+            $idx = 1;
+            foreach ($buttons as $button) {
+
+                /** @var qsbButton $button */
+                $phs = $button->toArray();
+                $phs['idx'] = $idx;
+                $phs['href'] = '';
+
+                // figure out the link
+                $btnAction = $button->get('action_id');
+                if (!empty($btnAction)) {
+                    $phs['href'] = '?a=' . $btnAction;
+                }
+                $btnActionKey = $button->get('action_key');
+                if (!empty($btnActionKey)) {
+                    $phs['href'] = '?a=' . $btnActionKey;
+
+                    $namespace = $button->get('action_namespace');
+                    if (!empty($namespace) && $namespace !== 'core') {
+                        $phs['href'] .= '&namespace=' . $namespace;
+                    }
+                }
+
+                $btnActionProps = $button->get('action_props');
+                if (!empty($btnActionProps) && (!empty($btnAction) || !empty($btnActionKey))) {
+                    $phs['href'] .= '&' . $btnActionProps;
+                }
+
+                $btnLink = $button->get('link');
+                if (!empty($btnLink)) {
+                    $phs['href'] = $btnLink;
+                }
+
+                // concat output
+                $output .= $this->qsb->getChunk('Dashboard/Item', $phs) . "\n";
+                $idx++;
+            }
+
+            if (!empty($output)) {
+                $phs = $set->toArray();
+                $phs['buttons'] = $output;
+                $output = $this->qsb->getChunk('Dashboard/Outer', $phs);
+            }
+        }
+
+        return $output;
+    }
+
+    /**
+     * @return null|object
+     */
+    public function getActiveSet()
     {
         $usrGroupIds = (array) $this->modx->user->getUserGroups();
-        $output = '';
 
         $c = $this->modx->newQuery('qsbSet');
         $c->leftJoin('qsbSetUserGroup', 'UserGroup');
@@ -54,67 +123,14 @@ class QuickstartButtonsDashboardWidget extends modDashboardWidgetInterface {
         $c->limit(1);
 
         $set = $this->modx->getObject('qsbSet', $c);
-        if(!empty($set) && is_object($set)) {
 
+        if ($set instanceof qsbSet) {
             $this->widget->set('name', $set->get('name'));
             $this->widget->set('description', $set->get('description'));
             $this->widget->set('size', $set->get('size'));
-
-            // collect all buttons in set
-            $c = $this->modx->newQuery('qsbButton');
-            $c->select(array('qsbButton.*', 'Icon.class AS iconcls', 'Icon.path as iconpath'));
-            $c->leftJoin('qsbIcon', 'Icon');
-            $c->where(array('active' => true));
-            $c->sortby('ranking ASC, id', 'ASC');
-
-            $buttons = $set->getMany('Button', $c);
-            if(!empty($buttons)) {
-
-                $idx = 1;
-                foreach($buttons as $button) {
-
-                    /** @var qsbButton $button */
-                    $phs = $button->toArray();
-                    $phs['idx'] = $idx;
-                    $phs['href'] = '';
-
-                    // figure out the link
-                    $btnAction = $button->get('action_id');
-                    if(!empty($btnAction)) {
-                        $phs['href'] = '?a='.$btnAction;
-                    }
-                    $btnActionKey = $button->get('action_key');
-                    if (!empty($btnActionKey)) {
-                        $phs['href'] = '?a=' . $btnActionKey;
-
-                        $namespace = $button->get('action_namespace');
-                        if (!empty($namespace) && $namespace !== 'core') {
-                            $phs['href'] .= '&namespace=' . $namespace;
-                        }
-                    }
-
-                    $btnActionProps = $button->get('action_props');
-                    if(!empty($btnActionProps) && (!empty($btnAction) || !empty($btnActionKey))) {
-                        $phs['href'] .= '&'.$btnActionProps;
-                    }
-
-                    $btnLink = $button->get('link');
-                    if(!empty($btnLink)) { $phs['href'] = $btnLink; }
-
-                    // concat output
-                    $output .= $this->qsb->getChunk('Dashboard/Item', $phs)."\n";
-                    $idx++;
-                }
-
-                if(!empty($output)) {
-                    $phs = $set->toArray();
-                    $phs['buttons'] = $output;
-                    $output = $this->qsb->getChunk('Dashboard/Outer', $phs);
-                }
-            }
         }
 
-        return $output;
+        return $set;
     }
 }
 
