528c3fa93515030873e4501fdabdebb8c1a9896b
Murray Wood <murray@digitalpenguin.studio>
2025-02-01 10:28:01 +0800

    Remove now defunct modAction from bootstrap script. Replace with namespaced modMenu.

--- _bootstrap/index.php
+++ _bootstrap/index.php
@@ -1,55 +1,55 @@
 <?php
 /* Get the core config */
-if (!file_exists(dirname(dirname(__FILE__)).'/config.core.php')) {
-    die('ERROR: missing '.dirname(dirname(__FILE__)).'/config.core.php file defining the MODX core path.');
+if (!file_exists(dirname(__FILE__, 2) .'/config.core.php')) {
+    die('ERROR: missing '. dirname(__FILE__, 2) .'/config.core.php file defining the MODX core path.');
 }
 
 echo "<pre>";
 /* Boot up MODX */
 echo "Loading modX...\n";
-require_once dirname(dirname(__FILE__)).'/config.core.php';
+require_once dirname(__FILE__, 2) .'/config.core.php';
 require_once MODX_CORE_PATH.'model/modx/modx.class.php';
 $modx = new modX();
 echo "Initializing manager...\n";
 $modx->initialize('mgr');
 $modx->getService('error','error.modError', '', '');
 
-$componentPath = dirname(dirname(__FILE__));
+$componentPath = dirname(__FILE__, 2);
 
-$QuickstartButtons = $modx->getService('quickstartbuttons','QuickstartButtons', $componentPath.'/core/components/quickstartbuttons/model/quickstartbuttons/', array(
+$QuickstartButtons = $modx->getService('quickstartbuttons','QuickstartButtons', $componentPath.'/core/components/quickstartbuttons/model/quickstartbuttons/', [
     'quickstartbuttons.core_path' => $componentPath.'/core/components/quickstartbuttons/',
-));
+]);
 
 
 /* Namespace */
-if (!createObject('modNamespace',array(
+if (!createObject('modNamespace', [
     'name' => 'quickstartbuttons',
     'path' => $componentPath.'/core/components/quickstartbuttons/',
     'assets_path' => $componentPath.'/assets/components/quickstartbuttons/',
-),'name', false)) {
+],'name', false)) {
     echo "Error creating namespace quickstartbuttons.\n";
 }
 
 /* Path settings */
-if (!createObject('modSystemSetting', array(
+if (!createObject('modSystemSetting', [
     'key' => 'quickstartbuttons.core_path',
     'value' => $componentPath.'/core/components/quickstartbuttons/',
     'xtype' => 'textfield',
     'namespace' => 'quickstartbuttons',
     'area' => 'Paths',
     'editedon' => time(),
-), 'key', false)) {
+], 'key', false)) {
     echo "Error creating quickstartbuttons.core_path setting.\n";
 }
 
-if (!createObject('modSystemSetting', array(
+if (!createObject('modSystemSetting', [
     'key' => 'quickstartbuttons.assets_path',
     'value' => $componentPath.'/assets/components/quickstartbuttons/',
     'xtype' => 'textfield',
     'namespace' => 'quickstartbuttons',
     'area' => 'Paths',
     'editedon' => time(),
-), 'key', false)) {
+], 'key', false)) {
     echo "Error creating quickstartbuttons.assets_path setting.\n";
 }
 
@@ -67,19 +67,19 @@ $bootstrapPos = strpos($requestUri, '_bootstrap/');
 $requestUri = rtrim(substr($requestUri, 0, $bootstrapPos), '/').'/';
 $assetsUrl = "{$url}{$requestUri}assets/components/quickstartbuttons/";
 
-if (!createObject('modSystemSetting', array(
+if (!createObject('modSystemSetting', [
     'key' => 'quickstartbuttons.assets_url',
     'value' => $assetsUrl,
     'xtype' => 'textfield',
     'namespace' => 'quickstartbuttons',
     'area' => 'Paths',
     'editedon' => time(),
-), 'key', false)) {
+], 'key', false)) {
     echo "Error creating quickstartbuttons.assets_url setting.\n";
 }
 
 // Create widget
-if (!createObject('modDashboardWidget', array(
+if (!createObject('modDashboardWidget', [
     'name' => 'quickstartbuttons.widget',
     'description' => 'quickstartbuttons.widget_desc',
     'type' => 'file',
@@ -87,48 +87,31 @@ if (!createObject('modDashboardWidget', array(
     'content' => '[[++core_path]]components/quickstartbuttons/model/quickstartbuttons/dashboardwidget.class.php',
     'namespace' => 'quickstartbuttons',
     'lexicon' => 'quickstartbuttons:dashboards',
-), 'name', false)) {
+], 'name', false)) {
     echo "Error creating quickstartbuttons.widget widget.\n";
 }
 
-// Create action
-if (!createObject('modAction', array(
+if (!createObject('modMenu', [
+    'text' => 'quickstartbuttons',
+    'parent' => 'components',
+    'description' => 'quickstartbuttons.menu_desc',
+    'icon' => 'images/icons/plugin.gif',
+    'menuindex' => 99,
     'namespace' => 'quickstartbuttons',
-    'parent' => 0,
-    'controller' => 'controllers/index',
-    'haslayout' => true,
-    'lang_topics' => 'quickstartbuttons'.':default',
-    'assets' => '',
-), array('namespace','controller'), false)) {
-    echo "Error creating modAction.\n";
-}
-
-$action = $modx->getObject('modAction', array('namespace' => 'quickstartbuttons', 'controller' => 'controllers/index'));
-if ($action) {
-    if (!createObject('modMenu', array(
-        'text' => 'quickstartbuttons',
-        'parent' => 'components',
-        'description' => 'quickstartbuttons.menu_desc',
-        'icon' => 'images/icons/plugin.gif',
-        'menuindex' => 99,
-        'params' => '',
-        'handler' => '',
-        'permissions' => 'components',
-        'action' => $action->get('id')
-    ), 'text', true)) {
-        echo "Error creating modMenu.\n";
-    }
+    'action' => 'home',
+], 'text', false)) {
+    echo "Error creating menu.\n";
 }
 
 $manager = $modx->getManager();
 
 /* Create the tables */
-$objectContainers = array(
+$objectContainers = [
     'qsbButton',
     'qsbIcon',
     'qsbSet',
     'qsbSetUserGroup',
-);
+];
 echo "Creating tables...\n";
 
 foreach ($objectContainers as $oC) {
@@ -136,19 +119,19 @@ foreach ($objectContainers as $oC) {
 }
 
 $manager->alterField('qsbButton', 'icon');
-$manager->addField('qsbButton', 'icon_ms', array('after' => 'icon'));
-$manager->addField('qsbButton', 'icon_file', array('after' => 'icon_ms'));
+$manager->addField('qsbButton', 'icon_ms', ['after' => 'icon']);
+$manager->addField('qsbButton', 'icon_file', ['after' => 'icon_ms']);
 // 2015-04-09
-$manager->addField('qsbButton', 'action_key', array('after' => 'action_id'));
-$manager->addField('qsbButton', 'action_namespace', array('after' => 'action_key'));
+$manager->addField('qsbButton', 'action_key', ['after' => 'action_id']);
+$manager->addField('qsbButton', 'action_namespace', ['after' => 'action_key']);
 
 if (isset($_GET['loadIcons'])) {
     echo "Loading icons..\n";
 
     $modx->loadClass('transport.xPDOTransport', XPDO_CORE_PATH, true, true);
-    $options = array(
+    $options = [
         xPDOTransport::PACKAGE_ACTION => xPDOTransport::ACTION_INSTALL
-    );
+    ];
     include $componentPath . '/_build/resolvers/resolve.icons.php';
 }
 else {
@@ -167,7 +150,7 @@ echo "Done.";
  * @param bool $update
  * @return bool
  */
-function createObject ($className = '', array $data = array(), $primaryField = '', $update = true) {
+function createObject ($className = '', array $data = [], $primaryField = '', $update = true) {
     global $modx;
     /* @var xPDOObject $object */
     $object = null;
@@ -175,13 +158,13 @@ function createObject ($className = '', array $data = array(), $primaryField = '
     /* Attempt to get the existing object */
     if (!empty($primaryField)) {
         if (is_array($primaryField)) {
-            $condition = array();
+            $condition = [];
             foreach ($primaryField as $key) {
                 $condition[$key] = $data[$key];
             }
         }
         else {
-            $condition = array($primaryField => $data[$primaryField]);
+            $condition = [$primaryField => $data[$primaryField]];
         }
         $object = $modx->getObject($className, $condition);
         if ($object instanceof $className) {
